f000bb5a85687b64d3f2c124c56d843ca5ff59d4
mulukadan <kadan.md@gmail.com>
2017-04-12 18:56:34 +0300

    html done

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,6 @@
 
-var add = function(num1, num2){
-  
-     return num1 + num2;
-};
-
-var subtract = function(num1, num2){
-     return num1 - num2;
-};
-
-var divide = function(num1, num2){
-     return num1 / num2;
-};
-
-var multiply = function(num1, num2){
-     return num1 * num2;
-};
-
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-      var result = add(number1, number2);
-    $("#output").text(result);
-  });
-     });
+
+
+  
+ });
