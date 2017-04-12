73db5f28c0b68210f0d959d572554fca4a91d554
mulukadan <kadan.md@gmail.com>
2017-04-12 20:54:39 +0300

    pingpong working well

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,24 @@ $(document).ready(function() {
   $("form#inpt").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#number").val());
-    alert(number1);
-    //$("#output").text(number1);
+
+    //$("#output").append("<li>"+number1+"</li>");
+    $("#output").append(pingpong(number1));
   });
+
+  var pingpong = function(num) {
+  $("#results").empty();
+    for(i = 1; i <= num; i++){
+      if(i%15 === 0){
+        $("#results").append("<li>pingpong</li>");
+      }else if(i%5 === 0){
+        $("#results").append("<li>Pong</li>");
+      }else if(i%3 === 0){
+        $("#results").append("<li>Ping</li>");
+      }else {
+         $("#results").append("<li>"+i+"</li>");
+      }
+    }
+    //return list;
+  };
  });
