f6312eedd4f95c04d9d1856076ac0689226d6d82
mulukadan <kadan.md@gmail.com>
2017-04-12 19:45:54 +0300

    input working

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 
 $(document).ready(function() {
 
-
-  
+  $("form#inpt").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number").val());
+    alert(number1);
+    //$("#output").text(number1);
+  });
  });
